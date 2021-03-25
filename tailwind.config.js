2d32928c8f2791a0c353782d914c40aed1bf853c
kailoon <chankailoon@gmail.com>
2021-03-25 22:44:26 +0800

    initial release

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+	purge: ['./pages/**/*.js', './components/**/*.js', './layouts/**/*.js'],
+	darkMode: 'media', // or 'media' or 'class'
+	theme: {
+		extend: {}
+	},
+	variants: {
+		extend: {}
+	},
+	plugins: []
 }
